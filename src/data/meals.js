9aa05dc9e17396a16d0289c6165615536379b968
Amanda Sporrer <amanda.sporrer@gmail.com>
2026-08-17 13:56:09 +0200

    AMA-56 create some meal data

---
+++ src/data/meals.js
@@ -0,0 +1,92 @@
+export const meals = [
+  {
+    id: 1,
+    name: "Spinat mit Kartoffeln und Ei",
+    ingredients: [
+      {
+        name: "Spinat",
+        quantity: 350,
+        unit: "g",
+        type: "TK",
+      },
+      {
+        name: "Kartoffeln",
+        quantity: 8,
+        unit: "Stück",
+        type: "Gemüse",
+      },
+      {
+        name: "Ei",
+        quantity: 2,
+        unit: "Stück",
+        type: "Tierprodukt",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Ofengemüse",
+    ingredients: [
+      {
+        name: "Kartoffeln",
+        quantity: 6,
+        unit: "Stück",
+        type: "Gemüse",
+      },
+      {
+        name: "Zucchini",
+        quantity: 1,
+        unit: "Stück",
+        type: "Gemüse",
+      },
+      {
+        name: "Paprika",
+        quantity: 1,
+        unit: "Stück",
+        type: "Gemüse",
+      },
+      {
+        name: "Fetakäse",
+        quantity: 1,
+        unit: "Stück",
+        type: "Milchprodukt",
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Wraps",
+    ingredients: [
+      {
+        name: "Hühnchen",
+        quantity: 400,
+        unit: "g",
+        type: "Tierprodukt",
+      },
+      {
+        name: "Wraps",
+        quantity: 1,
+        unit: "Stück",
+        type: "Getreideprodukt",
+      },
+      {
+        name: "Tomate",
+        quantity: 1,
+        unit: "Stück",
+        type: "Gemüse",
+      },
+      {
+        name: "Avocado",
+        quantity: 1,
+        unit: "Stück",
+        type: "Gemüse",
+      },
+      {
+        name: "Cheddar",
+        quantity: 1,
+        unit: "Stück",
+        type: "Milchprodukt",
+      },
+    ],
+  },
+];
