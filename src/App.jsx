d48c7c18b1a7b502e0dae1fda16b8779281ba7a2
Amanda Sporrer <amanda.sporrer@gmail.com>
2026-08-17 14:55:51 +0200

    AMA-60 display meal ingredients

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,19 @@ import { meals } from "./data/meals";
 import "./App.css";
 
 function App() {
-  return <h1>Grocery Helper</h1>;
+  return (
+    <>
+      <h1>Grocery Helper</h1>
+      {meals.map((meal) => (
+        <li>
+          <h3>{meal.name}</h3>
+          {meal.ingredients.map((ingredient) => (
+            <li>{ingredient.name}</li>
+          ))}
+        </li>
+      ))}
+    </>
+  );
 }
 
 export default App;
